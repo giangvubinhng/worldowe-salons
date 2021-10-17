6fbf7f080023e9ae42466aa9c4c85773e11e1531
giangvubinhng <giangvubinhn@vt.edu>
2021-10-17 15:43:23 -0400

    sign up works

--- Routes/user.js
+++ Routes/user.js
@@ -1,15 +1,14 @@
 const bcrypt = require('bcrypt');
 const db = require('../Models/database');
+require('dotenv').config();
 /**
  * User sign Up
  */
-var message = '';
-exports.signup = function async(req, res) {
-	message = '';
+exports.signup = async function (req, res) {
 	if (req.method == 'POST') {
 		const encryptedPassword = await bcrypt.hash(
 			req.body.password,
-			process.env.SALT_ROUNDS
+			parseInt(process.env.SALT_ROUNDS)
 		);
 		const email = req.body.email;
 		const shop_name = req.body.shop_name;
@@ -19,23 +18,25 @@ exports.signup = function async(req, res) {
 				console.log(err);
 				return;
 			} else if (user.length > 0) {
-				console.log('this email already exists');
-				return;
-			} else if ((user.length = 0)) {
-				if (email !== '' && password !== '') {
+				return res.send('this email already exists');
+			} else if (user.length === 0) {
+				if (email !== '' && req.body.password !== '') {
 					db.query(
-						'INSERT INTO user (email, password, shop_name, phone) VALUES (?, ?, ?, ?)',
+						'INSERT INTO users (email, password, shop_name, phone) VALUES (?, ?, ?, ?)',
 						[email, encryptedPassword, shop_name, phone],
 						(err, rows) => {
 							if (err) {
 								console.log(err);
 								return;
 							} else {
-								res.status(200).send('Signed up successfully');
+								return res.status(200).send('Signed up successfully');
 							}
 						}
 					);
 				}
+			} else {
+				console.log('idk');
+				return;
 			}
 		});
 	}
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+const user = require('./Routes/user.js');
 const port = process.env.PORT || 5000;
+// const db = require('./Models/database.js');
+
 require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true })); // New
 app.use(express.json()); // New
-
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
-
+app.post('/signup', user.signup);
 app.listen(port, () => {
 	console.log(`Example app listening at ${port}`);
 });
