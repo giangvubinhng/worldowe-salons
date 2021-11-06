67aae47eaf765b43f8fff0c7c1799c0ee7fa1cce
giangvubinhng <giangvubinhn@vt.edu>
2021-11-06 14:03:16 -0400

    comment and file name change

--- Routes/user.js
+++ Routes/user.js
@@ -1,17 +1,19 @@
 const bcrypt = require('bcrypt');
 const db = require('../Models/database');
-var jwt = require('jsonwebtoken');
-const mailService = require('../utils/VerificationEmail');
+const jwt = require('jsonwebtoken');
+const emailingService = require('../services/emailing.service.js');
 require('dotenv').config();
 /**
  * User sign Up
  */
 exports.signup = async function (req, res) {
 	if (req.method == 'POST') {
+		// Hash password
 		const encryptedPassword = await bcrypt.hash(
 			req.body.password,
 			parseInt(process.env.SALT_ROUNDS)
 		);
+		//data from front end
 		const email = req.body.email;
 		const shop_name = req.body.shop_name;
 		const phone = req.body.phone;
@@ -23,96 +25,107 @@ exports.signup = async function (req, res) {
 		const technicians = req.body.technicians;
 		const services = req.body.services;
 
-		db.query('SELECT * FROM users WHERE email = ?', [email], (err, user) => {
-			if (err) {
-				console.log(err);
-			} else if (user.length > 0) {
-				return res.send('this email already exists');
-			} else if (user.length === 0) {
-				if (email !== '' && req.body.password !== '') {
-					db.query(
-						'INSERT INTO users (email, password, shop_name, phone, activated) VALUES (?, ?, ?, ?, ?)',
-						[email, encryptedPassword, shop_name, phone, false],
-						(err, result) => {
-							if (err) {
-								console.log(err);
-							} else {
-								const user_id = result.insertId;
-								// Store location
-								db.query(
-									'INSERT INTO location (user_id, street, city, state, country, zip) VALUES (?, ?, ?, ?, ?, ?)',
-									[
-										user_id,
-										shop_street,
-										shop_city,
-										shop_state,
-										shop_country,
-										shop_zip,
-									],
-									(err, rows) => {
-										if (err) {
-											console.log(err);
-										} else {
-											console.log('added to location successfully');
-										}
-									}
-								);
-								// Technician of store
-								technicians.forEach((tech) => {
+		db.query(
+			'SELECT * FROM users WHERE email = ? limit 1',
+			[email],
+			(err, user) => {
+				if (err) {
+					console.log(err);
+				} else if (user.length > 0) {
+					return res.send('this email already exists');
+				} else if (user.length === 0) {
+					if (email !== '' && req.body.password !== '') {
+						db.query(
+							'INSERT INTO users (email, password, shop_name, phone, activated) VALUES (?, ?, ?, ?, ?)',
+							[email, encryptedPassword, shop_name, phone, false],
+							(err, result) => {
+								if (err) {
+									console.log(err);
+								} else {
+									const user_id = result.insertId;
+									// Store location
 									db.query(
-										'INSERT INTO technicians (technician_name, user_id) VALUES (?, ?)',
-										[tech.name, user_id],
+										'INSERT INTO location (user_id, street, city, state, country, zip) VALUES (?, ?, ?, ?, ?, ?)',
+										[
+											user_id,
+											shop_street,
+											shop_city,
+											shop_state,
+											shop_country,
+											shop_zip,
+										],
 										(err, rows) => {
 											if (err) {
 												console.log(err);
 											} else {
-												console.log('added to technician successfully');
+												console.log('added to location successfully');
 											}
 										}
 									);
-								});
-								// Services for store
-								services.forEach((service) => {
+									// Technician of store
+									technicians.forEach((tech) => {
+										db.query(
+											'INSERT INTO technicians (technician_name, user_id) VALUES (?, ?)',
+											[tech.name, user_id],
+											(err, rows) => {
+												if (err) {
+													console.log(err);
+												} else {
+													console.log('added to technician successfully');
+												}
+											}
+										);
+									});
+									// Services for store
+									services.forEach((service) => {
+										db.query(
+											'INSERT INTO services (user_id, service_name) VALUES (?, ?)',
+											[user_id, service.name],
+											(err, rows) => {
+												if (err) {
+													console.log(err);
+												} else {
+													console.log('added to technician successfully');
+												}
+											}
+										);
+									});
+									const token = jwt.sign(
+										{ email: req.body.email, id: user_id },
+										process.env.VERIFICATION_TOKEN
+									);
 									db.query(
-										'INSERT INTO services (user_id, service_name) VALUES (?, ?)',
-										[user_id, service.name],
-										(err, rows) => {
+										'INSERT INTO verification_token (user_id, token) VALUES (?, ?)',
+										[user_id, token],
+										(err) => {
 											if (err) {
 												console.log(err);
-											} else {
-												console.log('added to technician successfully');
 											}
 										}
 									);
-								});
-								const token = jwt.sign(
-									{ email: req.body.email, id: user_id },
-									process.env.VERIFICATION_TOKEN
-								);
-								db.query(
-									'INSERT INTO verification_token (user_id, token) VALUES (?, ?)',
-									[user_id, token],
-									(err) => {
-										if (err) {
-											console.log(err);
-										}
-									}
-								);
-								res
-									.status(200)
-									.send('Signed up successfully. Please check your email');
-								mailService.sendConfirmationEmail(shop_name, email, token);
+									res
+										.status(200)
+										.send('Signed up successfully. Please check your email');
+									emailingService.sendConfirmationEmail(
+										shop_name,
+										email,
+										token
+									);
+								}
 							}
-						}
-					);
+						);
+					}
+				} else {
+					console.log('idk');
+					return;
 				}
-			} else {
-				console.log('idk');
-				return;
 			}
-		});
+		);
 	}
 };
+/**
+ * Verify user after registration using email service
+ */
 exports.verifyUser = async (req, res) => {
 	db.query(
 		'SELECT user_id FROM verification_token WHERE token = ? limit 1',
---
+++ services/emailing.service.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transport = nodemailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS,
+	},
+});
+
+module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
+	transport
+		.sendMail({
+			from: process.env.EMAIL_USER,
+			to: email,
+			subject: 'Please confirm your account',
+			html: `
+          <h2>Hello ${name}</h2>
+          <p>Thank you for joining Worldowe. Please confirm your email by clicking on the following link</p>
+          <a href=http://localhost:3000/confirm/${confirmationCode}> Click here</a>
+          </div>`,
+		})
+		.catch((err) => console.log(err));
+};
