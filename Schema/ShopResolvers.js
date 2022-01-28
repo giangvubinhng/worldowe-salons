7bd1d79989778986e9c7a6add953fefad9860615
Giang Vu Binh Nguyen <giangvubinhn@vt.edu>
2022-01-28 14:41:10 -0500

    working on services and technicians

--- Schema/ShopResolvers.js
+++ Schema/ShopResolvers.js
@@ -1,20 +1,30 @@
-const shop = require('../Routes/salons');
-const {ForbiddenError} = require('apollo-server-express')
+const shop = require("../Routes/salons");
+const { ForbiddenError } = require("apollo-server-express");
 const shopResolvers = {
-	Query: {
-		async shops() {
-				return shop.getAllShops(); 
-		}, 
-	},
-	Mutation: {
-		createShop(_, args, context) {
-		if(!context.user){
-			throw new ForbiddenError("Unauthorized")
-		}
-			shop.createNewShop(context.user.id, args.shop);
-			return args.shop;
-		},
-	},
+  Query: {
+    async shops() {
+      return shop.getAllShops();
+    },
+  },
+  Mutation: {
+    createShop(_, args, context) {
+      if (!context.user) {
+        throw new ForbiddenError("Unauthorized");
+      }
+      shop.createNewShop(context.user.id, args.shop);
+      return args.shop;
+    },
+    addService(_, args, context) {
+      if (!context.user) {
+        throw new ForbiddenError("Unauthorized");
+      }
+    },
+    addTechnician(_, args, context) {
+      if (!context.user) {
+        throw new ForbiddenError("Unauthorized");
+      }
+    },
+  },
 };
 
 module.exports = { shopResolvers };
--- Schema/ShopTypeDefs.js
+++ Schema/ShopTypeDefs.js
@@ -1,39 +1,41 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const shopTypeDefs = gql`
-	# Types
-	type Shop {
-		user_id: Int
-		id: Int
-		shop_name: String!
-		street: String!
-		city: String!
-		state: String!
-		country: String!
-		zip: String!
-		phone: String!
-	}
+  # Types
+  type Shop {
+    user_id: Int
+    id: Int
+    shop_name: String!
+    street: String!
+    city: String!
+    state: String!
+    country: String!
+    zip: String!
+    phone: String!
+  }
 
-	# Input
-	input IShop{
-		shop_name: String!
-		street: String!
-		city: String!
-		state: String!
-		country: String!
-		zip: String!
-		phone: String!
-	}
+  # Input
+  input IShop {
+    shop_name: String!
+    street: String!
+    city: String!
+    state: String!
+    country: String!
+    zip: String!
+    phone: String!
+  }
 
-	# Queries
-	type Query {
-		shops: [Shop]
-	}
+  # Queries
+  type Query {
+    shops: [Shop]
+  }
 
-	# Mutations
-	type Mutation {
-		createShop(shop: IShop!): Shop
-	}
+  # Mutations
+  type Mutation {
+    createShop(shop: IShop!): Shop
+    addService(services: [String!]!): [String]!
+    addTechnician(techs: [String!]!): [String]!
+  }
 `;
 
 module.exports = { shopTypeDefs };
